735c3019e46cc6a414c7ef3fd21092615d584ca7
Sam Glerum <samuelglerum@hotmail.com>
2018-06-17 21:40:14 +0200

    working on setting up environment variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+const env = require('dotenv').config();
+const api_key = env.STEAM_API_KEY;
+console.log("versie: " +api_key);
+
+
 // Set the value of the input field to an empty string when a user clicks the field
 function removeFieldValue() {
     $('#steamID_input').val("");
@@ -9,5 +14,16 @@ function showSelectedGame() {
 
 function getListOfSteamGames() {
     let steamID = $('#steamID_input').val();
-    console.log(steamID);
+
+    $.ajax ({
+        url: 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + api_key + '&steamid=' + steamID + '&format=json',
+        dataType: 'JSON',
+
+        succes: function(data) {
+            console.log(data)
+        },
+        error: function(data) {
+            console.log("Error");
+        }
+    })
 }
